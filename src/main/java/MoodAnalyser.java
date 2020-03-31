e1d17d5c658c7bd81ca6163ea2a4cd80e874af87
prithvi0 <prithviy96@gmail.com>
2020-03-31 22:42:26 +0530

    Added program to analyse and respond happy and sad mood

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,5 +1,13 @@
 public class MoodAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyser");
+    private String message;
+    public MoodAnalyser(String message) {
+        this.message = message;
     }
-}
+
+    public String analyseMood() {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+} 
